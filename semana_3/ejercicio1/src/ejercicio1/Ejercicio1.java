8ef9eddb432ee15e26c8ce8baddfef7aa2c3ca26
F20LAB204E014 <F20LAB204E014@192.168.124.42>
2025-09-03 18:59:47 -0500

    Primer commit

---
+++ semana_3/ejercicio1/src/ejercicio1/Ejercicio1.java
@@ -0,0 +1,15 @@
+package ejercicio1;
+
+import java.util.Scanner;
+
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+    
+        System.out.print("Ingrese su nombre: ");
+        String nombre = sc.nextLine();
+        
+        System.out.println("Hola, " + nombre);
+    }
+    
+}
