2781c4a9127802abf889bcd2e8e6dde585b5f7de
F20LAB204E014 <F20LAB204E014@192.168.124.42>
2025-09-03 19:38:53 -0500

    Actualizando en repo

---
+++ semana_3/ejercicio1/src/ejercicio1/Curso.java
@@ -0,0 +1,44 @@
+package ejercicio1;
+
+public class Curso {
+    private String codigo;
+    private String nombre;
+    private int credito;
+
+    public Curso() {
+        codigo = " ";
+        nombre = " ";        
+        credito = 0;    
+    }
+    
+    public Curso(String codigo, String nombre, int credito) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.credito = credito;
+    }
+
+    public int getCredito() {
+        return credito;
+    }
+
+    public void setCredito(int credito) {
+        this.credito = credito;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+}
