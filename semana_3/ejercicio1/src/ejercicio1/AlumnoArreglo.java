51381b83994c7570cd3349a38cdd16dbf38acb4c
F20LAB204E014 <F20LAB204E014@192.168.124.42>
2025-09-03 19:38:47 -0500

    Actualizando en repo

---
+++ semana_3/ejercicio1/src/ejercicio1/AlumnoArreglo.java
@@ -0,0 +1,28 @@
+package ejercicio1;
+
+public class AlumnoArreglo {
+    // Metodos
+    private boolean estaVacio(){
+    
+    }
+    
+    private boolean estaLleno(){
+    
+    }
+    
+    public boolean agregar(Alumno alumno){
+    
+    
+    }
+    
+    public Alumno buscar(String codigo){
+    
+    
+    }
+
+    @Override
+    public String toString() {
+        return "AlumnoArreglo{" + '}';
+    }
+    
+}
