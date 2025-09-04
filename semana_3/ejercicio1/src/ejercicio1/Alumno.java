feacb6447b1292772dfa7bdba8f7856e0bf1f24b
F20LAB204E014 <F20LAB204E014@192.168.124.42>
2025-09-03 19:37:47 -0500

    Actualizando en repo

---
+++ semana_3/ejercicio1/src/ejercicio1/Alumno.java
@@ -0,0 +1,66 @@
+package ejercicio1;
+
+public class Alumno {
+    private String codigo;
+    private String nombre;
+    private String apellido;
+    private String dni;
+    private double promedio;
+    
+    public Alumno(){
+        codigo = " ";
+        nombre = " ";
+        apellido = " ";
+        dni = " ";
+        promedio = 0;
+    }
+    
+    public Alumno(String codigo, String nombre, String apellido, String dni, double promedio) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.dni = dni;
+        this.promedio = promedio;
+    }
+
+    public double getPromedio() {
+        return promedio;
+    }
+
+    public void setPromedio(double promedio) {
+        this.promedio = promedio;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+       
+}
